f1e35a63dc21e20a08952d60f3cb23b357b70549
kms <kmmin78@gmail.com>
2021-04-11 21:35:23 +0900

    test2

--- app-main/src/main/kotlin/kr/flab/wiki/app/WikiApplication.kt
+++ app-main/src/main/kotlin/kr/flab/wiki/app/WikiApplication.kt
@@ -3,5 +3,7 @@ package kr.flab.wiki.app
 class WikiApplication {
     fun start(args: List<String>) {
         println("Hello World! Current app profile is: ${AppConfig.PROFILE}")
+        println("test")
+        println("test2")
     }
 }
