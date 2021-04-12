6e7f9d0eaa54cc474a95a3d2ffeb17056f5f6375
kms <kmmin78@gmail.com>
2021-04-13 02:16:26 +0900

    1. executable jar build 2. apply detekt

--- app-main/src/local/kotlin/kr/flab/wiki/app/AppConfig.kt
+++ app-main/src/local/kotlin/kr/flab/wiki/app/AppConfig.kt
@@ -3,7 +3,9 @@ package kr.flab.wiki.app
 import kr.flab.wiki.core.AppProfile
 
 object AppConfig {
-    val APP_NAME = "awesome-wiki"
+    //detekt에서 Maybe Const 메세지 보여주면서 빌드 실패.
+    //val APP_NAME = "awesome-wiki"
+    const val APP_NAME = "awesome-wiki"
 
     val PROFILE = AppProfile.LOCAL
 }
--- app-main/src/main/kotlin/kr/flab/wiki/app/WikiApplication.kt
+++ app-main/src/main/kotlin/kr/flab/wiki/app/WikiApplication.kt
@@ -1,10 +1,15 @@
 package kr.flab.wiki.app
 
+import kr.flab.wiki.text.isNullOrUnicodeBlank
+
 class WikiApplication {
     fun start(args: List<String>) {
+
         println("Hello World! Current app profile is: ${AppConfig.PROFILE}")
-        println("test")
-        println("test2")
-        println("test3")
+        println("test : ${"test".isNullOrUnicodeBlank()}")
+        println("test2 : ${"test2".isNullOrUnicodeBlank()}")
+        println("test3 : ${"test3".isNullOrUnicodeBlank()}")
+        println("".isNullOrUnicodeBlank())
+
     }
 }
