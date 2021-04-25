56a70c7a80dcff9eb0c4349d3fed63105f8ae031
neropsys <neropsys@gmail.com>
2021-04-25 17:24:51 +0900

    인터페이스 정의 누락 부분 수정

--- app-core/src/main/kotlin/kr/flab/wiki/core/login/respository/UserLoginRepository.kt
+++ app-core/src/main/kotlin/kr/flab/wiki/core/login/respository/UserLoginRepository.kt
@@ -3,5 +3,5 @@ package kr.flab.wiki.core.login.respository
 import kr.flab.wiki.core.login.persistence.User
 
 interface UserLoginRepository {
-    fun findByIdWithPassword(user: User)
+    fun findByIdWithPassword(user: User):Boolean
 }
