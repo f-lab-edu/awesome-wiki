5d25dceebab534b7798dbeb912cfac2198458ff0
kms <kmmin78@gmail.com>
2021-04-26 22:38:33 +0900

    #9 로그인 기능
    각 케이스 별 테스트 코드 작성 완료.
    
    * given : 로그인 하지 않은 사용자는
    * when : 이메일/비밀번호 입력 후 로그인 버튼을 클릭했을 때
    * and : 이메일/비밀번호가 일치하는 회원이 있으면
    * expected/then : 세션에 유저 정보를 저장하고
    * and : 로그인에 성공한다.
    *
    * given : 로그인 하지 않은 사용자는
    * when : 이메일/비밀번호 입력 후 로그인 버튼을 클릭했을 때
    * and : 이메일이 이메일 형식이 아니면
    * expected/then : 로그인에 실패한다.
    *
    * given : 로그인 하지 않은 사용자는
    * when : 이메일/비밀번호 입력 후 로그인 버튼을 클릭했을 때
    * and : 이메일 혹은 비밀번호가 비어있으면
    * expected/then : 로그인에 실패한다.
    *
    * given : 로그인 하지 않은 사용자는
    * when : 이메일/비밀번호 입력 후 로그인 버튼을 클릭했을 때
    * and : 이메일/비밀번호가 일치하는 회원이 없으면
    * expected/then : 로그인에 실패한다.

--- app-core/src/main/java/kr/flab/wiki/core/login/business/UserLoginServiceImpl.java
+++ app-core/src/main/java/kr/flab/wiki/core/login/business/UserLoginServiceImpl.java
@@ -1,5 +1,6 @@
 package kr.flab.wiki.core.login.business;
 
+import kr.flab.wiki.core.login.LoginUtils;
 import kr.flab.wiki.core.login.exception.LoginException;
 import kr.flab.wiki.core.login.persistence.User;
 import kr.flab.wiki.core.login.repository.SessionRepository;
@@ -21,10 +22,35 @@ public class UserLoginServiceImpl implements UserLoginService {
     @Override
     public User login(User user) {
 
+        String email = user.getEmail();
+        String password = user.getPassword();
+
+        //이메일이 비어있을 경우
+        if(email.isEmpty()){
+            throw new LoginException("Email is Empty!");
+        }
+
+        //이메일 형식이 아닐 경우
+        if(!LoginUtils.isValidEmail(email)){
+            throw new LoginException("Not Email Type!");
+        }
+
+        //비밀번호가 비어있을 경우
+        if(password.isEmpty()){
+            throw new LoginException("Password is Empty!");
+        }
+
+        boolean isMatched = userLoginRepository.findByIdWithPassword(user);
+
+        //매치되는 회원이 없을 경우
+        if(!isMatched){
+            throw new LoginException("There's No Matched Member!");
+        }
+
         //세션 저장
-        if(sessionRepository.setAttribute("userName", user.getEmail()) == null){
+        if(sessionRepository.setAttribute("userName", email) == null){
             throw new LoginException("Session creation failed!");
         }
-        return userLoginRepository.findByIdWithPassword(user) ? user : null;
+        return isMatched ? user : null;
     }
 }
