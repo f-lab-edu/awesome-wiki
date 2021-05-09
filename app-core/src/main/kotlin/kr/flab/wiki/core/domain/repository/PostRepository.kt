f9d51571ac227b915074fbccd7c5da77c4256f9d
neropsys <neropsys@gmail.com>
2021-05-10 04:50:24 +0900

    현재 단계에서 불필요한 메서드 제거

--- app-core/src/main/kotlin/kr/flab/wiki/core/domain/repository/PostRepository.kt
+++ app-core/src/main/kotlin/kr/flab/wiki/core/domain/repository/PostRepository.kt
@@ -4,6 +4,5 @@ import kr.flab.wiki.core.domain.post.Post
 
 interface PostRepository {
     fun create(post: Post): Boolean
-    fun update(post: Post, mainText: String): Boolean
     fun get(title: String): Post?
 }
