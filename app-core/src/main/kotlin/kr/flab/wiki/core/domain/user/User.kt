f08054fcb5a7489dc3f473ffbf49a6cf20c49fca
neropsys <neropsys@gmail.com>
2021-07-05 07:33:56 +0900

    전역메소드 newInstance

--- app-core/src/main/kotlin/kr/flab/wiki/core/domain/user/User.kt
+++ app-core/src/main/kotlin/kr/flab/wiki/core/domain/user/User.kt
@@ -1,6 +1,7 @@
 package kr.flab.wiki.core.domain.user
 
 import kr.flab.wiki.core.common.annotation.DomainModel
+import kr.flab.wiki.core.domain.user.persistence.UserEntity
 import java.time.LocalDateTime
 
 @DomainModel
@@ -8,6 +9,7 @@ interface User {
     /**
      * 사용자 식별 수단, 한번 결정하면 다시는 바꿀 수 없음
      */
+
     val userName: String
 
     var emailAddress: String
@@ -15,4 +17,16 @@ interface User {
     val registeredAt: LocalDateTime
 
     var lastActiveAt: LocalDateTime
+
+    companion object {
+        val name: String = User::class.java.simpleName
+        fun newInstance(
+            userName: String,
+            emailAddress: String,
+            registeredAt: LocalDateTime,
+            lastActiveAt: LocalDateTime
+        ): User {
+            return UserEntity(userName, emailAddress, registeredAt, lastActiveAt)
+        }
+    }
 }
