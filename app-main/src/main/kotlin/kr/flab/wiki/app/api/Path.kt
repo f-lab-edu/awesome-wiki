454d71007f99da1040f243232939d120612129b6
neropsys <neropsys@gmail.com>
2021-07-05 07:41:56 +0900

    path + 유저api 구현

---
+++ app-main/src/main/kotlin/kr/flab/wiki/app/api/Path.kt
@@ -0,0 +1,5 @@
+package kr.flab.wiki.app.api
+
+object Path {
+    const val USER = "/user"
+}
--- app-main/src/main/kotlin/kr/flab/wiki/app/api/user/registerUser/RegisterUserApi.kt
+++ app-main/src/main/kotlin/kr/flab/wiki/app/api/user/registerUser/RegisterUserApi.kt
@@ -1,17 +1,27 @@
 package kr.flab.wiki.app.api.user.registerUser
 
+import kr.flab.wiki.app.api.Path
 import kr.flab.wiki.app.api.user.UserResource
 import kr.flab.wiki.app.type.annotation.ApiHandler
 import kr.flab.wiki.app.type.annotation.ApiRequest
 import kr.flab.wiki.core.domain.user.UserService
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
+@Controller
+@RequestMapping(Path.USER)
 @ApiHandler
 class RegisterUserApi(
     private val userService: UserService
 ) {
+    @GetMapping(
+        path = ["register"],
+        produces = ["application/json"])
     fun onRequest(
-        @ApiRequest userName: String,
-        @ApiRequest emailAddress: String
+        @ApiRequest @RequestParam userName: String,
+        @ApiRequest @RequestParam emailAddress: String
     ): UserResource {
         val registeredUser = userService.registerUser(userName, emailAddress)
 
